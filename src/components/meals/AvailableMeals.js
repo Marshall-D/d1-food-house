98103c616800521c7f8a8a42210bb0cd78a6fe09
D1 <d1megaenterprise@gmail.com>
2021-04-15 17:47:18 +0100

    added individual Mealitem component

--- src/components/meals/AvailableMeals.js
+++ src/components/meals/AvailableMeals.js
@@ -1,14 +1,27 @@
 import DUMMY_MEALS from "./DummyMeals";
 import classes from './AvailableMeals.module.css'
+import Card from "../ui/Card";
+import MealItem from "./MealItem/MealItem";
 
 
 const AvailableMeals = () => {
-    const mealList = DUMMY_MEALS.map( meal => <li>{meal.name}</li> );
+    const mealList = DUMMY_MEALS.map( meal =>
+        <MealItem
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        /> );
+
+
     return (
         <section className={classes.meals}>
-            <ul>
-                {mealList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealList}
+                </ul>
+            </Card>
+
         </section>
 
     );
---
+++ src/components/meals/MealItem/MealItemForm.js
@@ -0,0 +1,15 @@
+import React,{Fragment} from 'react';
+
+const MealItemForm = (props) => {
+
+    return (
+        <Fragment>
+            <div>
+                <h2>Let's get started!</h2>
+            </div>
+        </Fragment>
+
+    );
+}
+
+export default MealItemForm;
