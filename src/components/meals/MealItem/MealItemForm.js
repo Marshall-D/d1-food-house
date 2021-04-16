2bfa5e30a7c931e3efe1e47842f75441b3f2d809
D1 <d1megaenterprise@gmail.com>
2021-04-16 19:42:18 +0100

    added form for each item

--- src/components/meals/MealItem/MealItemForm.js
+++ src/components/meals/MealItem/MealItemForm.js
@@ -1,13 +1,20 @@
-import React,{Fragment} from 'react';
+import classes from './MealItemForm.module.css'
+import Input from "../../ui/Input";
 
 const MealItemForm = (props) => {
 
     return (
-        <Fragment>
-            <div>
-                <h2>Let's get started!</h2>
-            </div>
-        </Fragment>
+        <form className={classes.form}>
+            <Input label="Amount" input={{
+    id: 'amount',
+    type: 'number',
+    min: '1',
+    max: '5',
+    step: '1',
+    defaultValue: '1'
+}} />
+            <button>+ Add</button>
+        </form>
 
     );
 }
