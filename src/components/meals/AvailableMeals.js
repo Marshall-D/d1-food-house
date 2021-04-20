f7f894464654d189f9d3dffa7c4f00209ade7603
D1 <d1megaenterprise@gmail.com>
2021-04-20 10:20:06 +0100

    starting the fetch action

--- src/components/meals/AvailableMeals.js
+++ src/components/meals/AvailableMeals.js
@@ -2,9 +2,13 @@ import DUMMY_MEALS from "./DummyMeals";
 import classes from './AvailableMeals.module.css'
 import Card from "../ui/Card";
 import MealItem from "./MealItem/MealItem";
-
+import {useEffect} from 'react';
 
 const AvailableMeals = () => {
+    useEffect(() => {
+        fetch('https://react-my-burger-90039-default-rtdb.firebaseio.com/meals.json');
+    },[]);
+
     const mealList = DUMMY_MEALS.map( meal =>
         <MealItem
             key={meal.id}
